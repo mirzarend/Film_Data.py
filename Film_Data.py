0ea51878bbcfcf658b25a96122544210551df599
Mirza <mirzar01052008@gmail.com>
2026-04-24 23:58:00 +0700

    Add files via upload

--- Film_Data.py
+++ Film_Data.py
@@ -1,40 +1,61 @@
-#pertanyaan pakai dictionary
-pertanyaan = ["2 × 5 =", "4 × 8 =","7 × 9 ="]
+listFilm = [
+  {
+    "judul" : "The Doom",
+    "genre" : "Horror",
+    "rating" : 8.30,
+    "tahun" : 2018
+  },
+  {
+    "judul" : "Spiderman",
+    "genre" : "Adventure",
+    "rating" : 9.52,
+    "tahun" : 2020
+  },
+  {
+    "judul" : "Z Town",
+    "genre" : "Zombie",
+    "rating" : 7.53,
+    "tahun" : 2008
+  },
+  ]
 
-#dictionary jawaban
-jawaban = ["10","32","63"]
+def tampilkan_film(data) :
+  for film in data :
+    print("="*30)
+    print(f"Judul : {film["judul"]}")
+    print(f"Genre : {film["genre"]}")
+    print(f"Rating : {film["rating"]}")
+    print(f"Tahun : {film["tahun"]}")
+    print("="*30)
 
-def semuaPertanyaan(pertanyaan) : #sebenarnya ga pakai def juga gapapa saya rasa
-  print("="*30)
-  print("Semua pertanyaan :")
-  print(pertanyaan[0])
-  print(pertanyaan[1])
-  print(pertanyaan[2])
-  print("="*30)
+def film_terbaik(data):
+  rating_tertinggi = data[0]
+  for film in data :
+    if film["rating"] > rating_tertinggi["rating"] :
+      rating_tertinggi = film
+  return rating_tertinggi
 
-semuaPertanyaan(pertanyaan)
+def cari_genre(data) :
+  pilihanGenre = input("Pilih Genre")
+  ketemu = False
+  for film in data :
+    if film["genre"] == pilihanGenre :
+      ketemu = True
+      print(f"Film dengan genre {film["genre"]}, adalah : {film["judul"]}")
 
-#input dan validasi
-while True :
-  try :
-   pilihPertanyaan = int(input("Pilih pertanyaan 1-3 :"))
-   if 1 <= pilihPertanyaan <= 3 :
-     break
-   else :
-     print("harus angka 1-3")
-  except :
-    print("harus angka dan angka 1-3")
+  if not ketemu :
+      print("tidak ada film dengan genre tersebut")
 
-print("anda memilih pertanyaan ke -", pilihPertanyaan)
+print("="*30)
+print("----Data film----")
+print("="*30)
+tampilkan_film(listFilm)
+rating_tertinggi = film_terbaik(listFilm)
+print(f"Film dengan rating tertinggi adalah :{rating_tertinggi["judul"]}")
+print(f"Ratingnya adalah : {rating_tertinggi["rating"]}")
+print("="*30)
+cari_genre(listFilm)
+print("="*30)
 
-def tanyapertanyaan(pilihPertanyaan) :
-  jawabanPertanyaan = input(pertanyaan[pilihPertanyaan -1] + ':')
-  while True :
-    if jawabanPertanyaan == jawaban[pilihPertanyaan - 1] :
-      print("Benar!")
-      break
-    else :
-      jawabanPertanyaan = input("Kurang tepat!, coba lagi :")
 
-tanyapertanyaan(pilihPertanyaan)
   
